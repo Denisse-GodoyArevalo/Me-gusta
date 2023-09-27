97998555b615bb569479648728595a7abd84ba76
Denisse-GodoyArevalo <d.arevalogodoy@gmail.com>
2023-09-27 16:51:23 -0300

    Actividad terminada

---
+++ script.js
@@ -0,0 +1,6 @@
+function DarLike(){
+    let contadorDelike = document.querySelector("#numeroDeLike-3");
+    let addlike = contadorDelike.innerText;
+    addlike++
+    contadorDelike = addlike.innerText;
+}
